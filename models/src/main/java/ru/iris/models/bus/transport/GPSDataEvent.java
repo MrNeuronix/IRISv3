1077501387daa1b4862a64fdfc956a1e1f773c6d
nikolay.viguro <nikolay.viguro@loyaltyplant.com>
2018-04-18 17:15:06 +0300

    GPS track save on server side

--- models/src/main/java/ru/iris/models/bus/transport/GPSDataEvent.java
+++ models/src/main/java/ru/iris/models/bus/transport/GPSDataEvent.java
@@ -15,19 +15,16 @@ public class GPSDataEvent extends AbstractTransportEvent {
     private Double longitude;
     private Double elevation;
     private Double speed;
-    private Integer satellites;
-    private Instant time = Instant.now();
+    private Instant time;
 
     @Builder
-    public GPSDataEvent(Double latitude, Double longitude, Double speed, Double elevation, Instant time,
-                        Integer satellites, int id) {
+    public GPSDataEvent(Double latitude, Double longitude, Double speed, Double elevation, Instant time, int id) {
         super(id);
         this.latitude = latitude;
         this.longitude = longitude;
         this.speed = speed;
         this.elevation = elevation;
-        this.time = time == null ? Instant.now() : time;
-        this.satellites = satellites;
+        this.time = time;
         this.transportId = id;
     }
 }
